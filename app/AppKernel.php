d92ecebdf29ae469647b3781d67b613355f9955b
jbourdic <julien.bourdic@gmail.com>
2014-11-17 13:43:44 +0100

    Security changes for Oauth

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,9 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new JB\PlannedBundle\JBPlannedBundle(),
+            new JB\UserBundle\JBUserBundle(),
+            new HWI\Bundle\OAuthBundle\HWIOAuthBundle(),
+            new Braincrafted\Bundle\BootstrapBundle\BraincraftedBootstrapBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
